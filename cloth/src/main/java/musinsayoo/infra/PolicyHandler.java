af549c715f48d7ad8095644b2baeb883aba480a0
yoo-hyungi <149982799+yoo-hyungi@users.noreply.github.com>
2023-11-10 00:18:46 +0900

    commit

---
+++ cloth/src/main/java/musinsayoo/infra/PolicyHandler.java
@@ -0,0 +1,58 @@
+package musinsayoo.infra;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.naming.NameParser;
+import javax.naming.NameParser;
+import javax.transaction.Transactional;
+import musinsayoo.config.kafka.KafkaProcessor;
+import musinsayoo.domain.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+//<<< Clean Arch / Inbound Adaptor
+@Service
+@Transactional
+public class PolicyHandler {
+
+    @Autowired
+    InventoryRepository inventoryRepository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whatever(@Payload String eventString) {}
+
+    @StreamListener(
+        value = KafkaProcessor.INPUT,
+        condition = "headers['type']=='DeliveryStarted'"
+    )
+    public void wheneverDeliveryStarted_DecreaseCloth(
+        @Payload DeliveryStarted deliveryStarted
+    ) {
+        DeliveryStarted event = deliveryStarted;
+        System.out.println(
+            "\n\n##### listener DecreaseCloth : " + deliveryStarted + "\n\n"
+        );
+
+        // Sample Logic //
+        Inventory.decreaseCloth(event);
+    }
+
+    @StreamListener(
+        value = KafkaProcessor.INPUT,
+        condition = "headers['type']=='DeliveryReturned'"
+    )
+    public void wheneverDeliveryReturned_IncreaseCloth(
+        @Payload DeliveryReturned deliveryReturned
+    ) {
+        DeliveryReturned event = deliveryReturned;
+        System.out.println(
+            "\n\n##### listener IncreaseCloth : " + deliveryReturned + "\n\n"
+        );
+
+        // Sample Logic //
+        Inventory.increaseCloth(event);
+    }
+}
+//>>> Clean Arch / Inbound Adaptor
